cf4395af9e28182ca106a7f97136d96391e3a638
coltoutram <coltoutram@gmail.com>
2018-06-07 23:25:38 +0200

    Upload ping-time and fix spelling error.

---
+++ ping-time/pingtime.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+
+
+class Pingtime:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def pingtime(self, ctx):
+        """Ping pong."""
+        latencies = self.bot.latencies
+        msg = "Pong!\n"
+        for shard, pingt in latencies:
+            msg += "Shard {}/{}: {}ms\n".format(shard + 1, len(latencies), round(pingt*1000))
+        await ctx.send(msg)
